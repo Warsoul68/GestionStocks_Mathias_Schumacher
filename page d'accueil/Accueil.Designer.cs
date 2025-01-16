a35e155538ffc50fc03bc4f32286cc2f34d43378
Mathias.Schumacher <guzmer68@gmail.com>
2025-01-16 16:44:46 +0100

    Création de la page d'accueil de l'application, plus création de la barre d'outils

---
+++ page d'accueil/Accueil.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace page_d_accueil
+{
+    partial class Accueil
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tsMenuApplication = new System.Windows.Forms.ToolStrip();
+            this.tsParametres = new System.Windows.Forms.ToolStripDropDownButton();
+            this.listeDesCatégoriesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.visualisationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.créationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.miseÀJourToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.suppressionDeCatégorieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listeDesUnitéToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.visualisationToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.créationToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.miseÀJourToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.suppressionDunitéToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsDonnee = new System.Windows.Forms.ToolStripDropDownButton();
+            this.lToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listeMulticritèreDeRechercheDunEnsembleDarticlesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listeMulticritèresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsStock = new System.Windows.Forms.ToolStripDropDownButton();
+            this.listeMulticritèresDesNiveauxDeStockToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listeMultiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listeMulticritèresDesMouvementsDeStockToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsMenuApplication.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tsMenuApplication
+            // 
+            this.tsMenuApplication.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsParametres,
+            this.tsDonnee,
+            this.tsStock});
+            this.tsMenuApplication.Location = new System.Drawing.Point(0, 0);
+            this.tsMenuApplication.Name = "tsMenuApplication";
+            this.tsMenuApplication.Size = new System.Drawing.Size(800, 25);
+            this.tsMenuApplication.TabIndex = 0;
+            this.tsMenuApplication.Text = "toolStrip1";
+            // 
+            // tsParametres
+            // 
+            this.tsParametres.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.listeDesCatégoriesToolStripMenuItem,
+            this.listeDesUnitéToolStripMenuItem});
+            this.tsParametres.Name = "tsParametres";
+            this.tsParametres.Size = new System.Drawing.Size(79, 22);
+            this.tsParametres.Text = "Paramètres";
+            // 
+            // listeDesCatégoriesToolStripMenuItem
+            // 
+            this.listeDesCatégoriesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.visualisationToolStripMenuItem,
+            this.créationToolStripMenuItem,
+            this.miseÀJourToolStripMenuItem,
+            this.suppressionDeCatégorieToolStripMenuItem});
+            this.listeDesCatégoriesToolStripMenuItem.Name = "listeDesCatégoriesToolStripMenuItem";
+            this.listeDesCatégoriesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.listeDesCatégoriesToolStripMenuItem.Text = "Liste des catégories";
+            // 
+            // visualisationToolStripMenuItem
+            // 
+            this.visualisationToolStripMenuItem.Name = "visualisationToolStripMenuItem";
+            this.visualisationToolStripMenuItem.Size = new System.Drawing.Size(205, 22);
+            this.visualisationToolStripMenuItem.Text = "visualisation";
+            // 
+            // créationToolStripMenuItem
+            // 
+            this.créationToolStripMenuItem.Name = "créationToolStripMenuItem";
+            this.créationToolStripMenuItem.Size = new System.Drawing.Size(205, 22);
+            this.créationToolStripMenuItem.Text = "création";
+            // 
+            // miseÀJourToolStripMenuItem
+            // 
+            this.miseÀJourToolStripMenuItem.Name = "miseÀJourToolStripMenuItem";
+            this.miseÀJourToolStripMenuItem.Size = new System.Drawing.Size(205, 22);
+            this.miseÀJourToolStripMenuItem.Text = "mise à jour";
+            // 
+            // suppressionDeCatégorieToolStripMenuItem
+            // 
+            this.suppressionDeCatégorieToolStripMenuItem.Name = "suppressionDeCatégorieToolStripMenuItem";
+            this.suppressionDeCatégorieToolStripMenuItem.Size = new System.Drawing.Size(205, 22);
+            this.suppressionDeCatégorieToolStripMenuItem.Text = "suppression de catégorie";
+            // 
+            // listeDesUnitéToolStripMenuItem
+            // 
+            this.listeDesUnitéToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.visualisationToolStripMenuItem1,
+            this.créationToolStripMenuItem1,
+            this.miseÀJourToolStripMenuItem1,
+            this.suppressionDunitéToolStripMenuItem});
+            this.listeDesUnitéToolStripMenuItem.Name = "listeDesUnitéToolStripMenuItem";
+            this.listeDesUnitéToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.listeDesUnitéToolStripMenuItem.Text = "Liste des unité";
+            // 
+            // visualisationToolStripMenuItem1
+            // 
+            this.visualisationToolStripMenuItem1.Name = "visualisationToolStripMenuItem1";
+            this.visualisationToolStripMenuItem1.Size = new System.Drawing.Size(177, 22);
+            this.visualisationToolStripMenuItem1.Text = "visualisation";
+            // 
+            // créationToolStripMenuItem1
+            // 
+            this.créationToolStripMenuItem1.Name = "créationToolStripMenuItem1";
+            this.créationToolStripMenuItem1.Size = new System.Drawing.Size(177, 22);
+            this.créationToolStripMenuItem1.Text = "création";
+            // 
+            // miseÀJourToolStripMenuItem1
+            // 
+            this.miseÀJourToolStripMenuItem1.Name = "miseÀJourToolStripMenuItem1";
+            this.miseÀJourToolStripMenuItem1.Size = new System.Drawing.Size(177, 22);
+            this.miseÀJourToolStripMenuItem1.Text = "mise à jour";
+            // 
+            // suppressionDunitéToolStripMenuItem
+            // 
+            this.suppressionDunitéToolStripMenuItem.Name = "suppressionDunitéToolStripMenuItem";
+            this.suppressionDunitéToolStripMenuItem.Size = new System.Drawing.Size(177, 22);
+            this.suppressionDunitéToolStripMenuItem.Text = "suppression d\'unité";
+            // 
+            // tsDonnee
+            // 
+            this.tsDonnee.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lToolStripMenuItem,
+            this.listeMulticritèreDeRechercheDunEnsembleDarticlesToolStripMenuItem,
+            this.listeMulticritèresToolStripMenuItem});
+            this.tsDonnee.Name = "tsDonnee";
+            this.tsDonnee.Size = new System.Drawing.Size(66, 22);
+            this.tsDonnee.Text = "Données";
+            // 
+            // lToolStripMenuItem
+            // 
+            this.lToolStripMenuItem.Name = "lToolStripMenuItem";
+            this.lToolStripMenuItem.Size = new System.Drawing.Size(369, 22);
+            this.lToolStripMenuItem.Text = "Liste multicritère de recherche d\'un fabricant";
+            // 
+            // listeMulticritèreDeRechercheDunEnsembleDarticlesToolStripMenuItem
+            // 
+            this.listeMulticritèreDeRechercheDunEnsembleDarticlesToolStripMenuItem.Name = "listeMulticritèreDeRechercheDunEnsembleDarticlesToolStripMenuItem";
+            this.listeMulticritèreDeRechercheDunEnsembleDarticlesToolStripMenuItem.Size = new System.Drawing.Size(369, 22);
+            this.listeMulticritèreDeRechercheDunEnsembleDarticlesToolStripMenuItem.Text = "Liste multicritère de recherche d\'un ensemble d\'articles";
+            // 
+            // listeMulticritèresToolStripMenuItem
+            // 
+            this.listeMulticritèresToolStripMenuItem.Name = "listeMulticritèresToolStripMenuItem";
+            this.listeMulticritèresToolStripMenuItem.Size = new System.Drawing.Size(369, 22);
+            this.listeMulticritèresToolStripMenuItem.Text = "Liste multicritère de recherche d\'un ensemble de dépots";
+            // 
+            // tsStock
+            // 
+            this.tsStock.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.listeMulticritèresDesNiveauxDeStockToolStripMenuItem,
+            this.listeMultiToolStripMenuItem,
+            this.listeMulticritèresDesMouvementsDeStockToolStripMenuItem});
+            this.tsStock.Name = "tsStock";
+            this.tsStock.Size = new System.Drawing.Size(49, 22);
+            this.tsStock.Text = "Stock";
+            // 
+            // listeMulticritèresDesNiveauxDeStockToolStripMenuItem
+            // 
+            this.listeMulticritèresDesNiveauxDeStockToolStripMenuItem.Name = "listeMulticritèresDesNiveauxDeStockToolStripMenuItem";
+            this.listeMulticritèresDesNiveauxDeStockToolStripMenuItem.Size = new System.Drawing.Size(279, 22);
+            this.listeMulticritèresDesNiveauxDeStockToolStripMenuItem.Text = "Liste multicritères des niveaux de stock";
+            // 
+            // listeMultiToolStripMenuItem
+            // 
+            this.listeMultiToolStripMenuItem.Name = "listeMultiToolStripMenuItem";
+            this.listeMultiToolStripMenuItem.Size = new System.Drawing.Size(308, 22);
+            this.listeMultiToolStripMenuItem.Text = "Liste multicritères pour inventaire";
+            // 
+            // listeMulticritèresDesMouvementsDeStockToolStripMenuItem
+            // 
+            this.listeMulticritèresDesMouvementsDeStockToolStripMenuItem.Name = "listeMulticritèresDesMouvementsDeStockToolStripMenuItem";
+            this.listeMulticritèresDesMouvementsDeStockToolStripMenuItem.Size = new System.Drawing.Size(308, 22);
+            this.listeMulticritèresDesMouvementsDeStockToolStripMenuItem.Text = "Liste multicritères des mouvements de stock";
+            // 
+            // Accueil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tsMenuApplication);
+            this.Name = "Accueil";
+            this.Text = "Accueil";
+            this.tsMenuApplication.ResumeLayout(false);
+            this.tsMenuApplication.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip tsMenuApplication;
+        private System.Windows.Forms.ToolStripDropDownButton tsParametres;
+        private System.Windows.Forms.ToolStripMenuItem listeDesCatégoriesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem visualisationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem créationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem miseÀJourToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem suppressionDeCatégorieToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listeDesUnitéToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem visualisationToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem créationToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem miseÀJourToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem suppressionDunitéToolStripMenuItem;
+        private System.Windows.Forms.ToolStripDropDownButton tsDonnee;
+        private System.Windows.Forms.ToolStripMenuItem lToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listeMulticritèreDeRechercheDunEnsembleDarticlesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listeMulticritèresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripDropDownButton tsStock;
+        private System.Windows.Forms.ToolStripMenuItem listeMulticritèresDesNiveauxDeStockToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listeMultiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listeMulticritèresDesMouvementsDeStockToolStripMenuItem;
+    }
+}
+
