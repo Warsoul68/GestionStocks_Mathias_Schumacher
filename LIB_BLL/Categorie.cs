b05ac757c2f336b70afc62a89aa69779c444e3b2
Mathias.Schumacher <guzmer68@gmail.com>
2025-02-06 16:54:20 +0100

    Création des objet

---
+++ LIB_BLL/Categorie.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIB_BLL
+{
+    public class Categorie
+    {
+        private string codeCategorie;
+        private string libelleCC;
+
+        public Categorie(string codeCategorie, string libelleCC)
+        {
+            this.codeCategorie = codeCategorie;
+            this.libelleCC = libelleCC;
+        }
+
+        public string getCodeCategorie()
+        {
+            return codeCategorie;
+        }
+
+        public void setCodeCategorie(string codeCategorie)
+        {
+            this.codeCategorie = codeCategorie;
+        }
+
+        public string getLibelleCC()
+        {
+            return libelleCC;
+        }
+
+        public void setLibelleCC(string libelleCC)
+        {
+            this.libelleCC = libelleCC;
+        }
+    }
+}
