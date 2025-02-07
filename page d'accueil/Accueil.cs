4d799b84776e67dc991c17982d2c853963a82961
Mathias.Schumacher <guzmer68@gmail.com>
2025-02-07 09:40:21 +0100

    Petite modif

--- page d'accueil/Accueil.cs
+++ page d'accueil/Accueil.cs
@@ -21,8 +21,6 @@ namespace page_d_accueil
 
         private void Accueil_Load(object sender, EventArgs e)
         {
-            // TODO: cette ligne de code charge les données dans la table 'bD_STOCK_SCHUMACHERDataSet.Article'. Vous pouvez la déplacer ou la supprimer selon les besoins.
-            this.articleTableAdapter.Fill(this.bD_STOCK_SCHUMACHERDataSet.Article);
             Connexion C = new Connexion();
             C.ShowDialog();
 
