6f95623de757bf663d5f38c2f81d087885848bb2
Mathias.Schumacher <guzmer68@gmail.com>
2025-05-06 08:44:40 +0200

    Test depot

--- page d'accueil/FrNouvelArticle.Designer.cs
+++ page d'accueil/FrNouvelArticle.Designer.cs
@@ -45,101 +45,114 @@
             // lblRef
             // 
             this.lblRef.AutoSize = true;
-            this.lblRef.Location = new System.Drawing.Point(12, 19);
+            this.lblRef.Location = new System.Drawing.Point(9, 15);
+            this.lblRef.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblRef.Name = "lblRef";
-            this.lblRef.Size = new System.Drawing.Size(76, 16);
+            this.lblRef.Size = new System.Drawing.Size(63, 13);
             this.lblRef.TabIndex = 0;
             this.lblRef.Text = "Référence :";
             // 
             // txtRef
             // 
-            this.txtRef.Location = new System.Drawing.Point(94, 16);
+            this.txtRef.Location = new System.Drawing.Point(70, 13);
+            this.txtRef.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtRef.Name = "txtRef";
-            this.txtRef.Size = new System.Drawing.Size(139, 22);
+            this.txtRef.Size = new System.Drawing.Size(105, 20);
             this.txtRef.TabIndex = 1;
             // 
             // lblLibelle
             // 
             this.lblLibelle.AutoSize = true;
-            this.lblLibelle.Location = new System.Drawing.Point(263, 22);
+            this.lblLibelle.Location = new System.Drawing.Point(197, 18);
+            this.lblLibelle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblLibelle.Name = "lblLibelle";
-            this.lblLibelle.Size = new System.Drawing.Size(53, 16);
+            this.lblLibelle.Size = new System.Drawing.Size(43, 13);
             this.lblLibelle.TabIndex = 2;
             this.lblLibelle.Text = "Libellé :";
             // 
             // txtLibelle
             // 
-            this.txtLibelle.Location = new System.Drawing.Point(322, 19);
+            this.txtLibelle.Location = new System.Drawing.Point(242, 15);
+            this.txtLibelle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtLibelle.Name = "txtLibelle";
-            this.txtLibelle.Size = new System.Drawing.Size(195, 22);
+            this.txtLibelle.Size = new System.Drawing.Size(147, 20);
             this.txtLibelle.TabIndex = 3;
             // 
             // lblCategorie
             // 
             this.lblCategorie.AutoSize = true;
-            this.lblCategorie.Location = new System.Drawing.Point(12, 65);
+            this.lblCategorie.Location = new System.Drawing.Point(9, 53);
+            this.lblCategorie.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblCategorie.Name = "lblCategorie";
-            this.lblCategorie.Size = new System.Drawing.Size(72, 16);
+            this.lblCategorie.Size = new System.Drawing.Size(58, 13);
             this.lblCategorie.TabIndex = 4;
             this.lblCategorie.Text = "Catégorie :";
             // 
             // cbCategorie
             // 
             this.cbCategorie.FormattingEnabled = true;
-            this.cbCategorie.Location = new System.Drawing.Point(90, 62);
+            this.cbCategorie.Location = new System.Drawing.Point(68, 50);
+            this.cbCategorie.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cbCategorie.Name = "cbCategorie";
-            this.cbCategorie.Size = new System.Drawing.Size(143, 24);
+            this.cbCategorie.Size = new System.Drawing.Size(108, 21);
             this.cbCategorie.TabIndex = 5;
             // 
             // lblFabricant
             // 
             this.lblFabricant.AutoSize = true;
-            this.lblFabricant.Location = new System.Drawing.Point(263, 65);
+            this.lblFabricant.Location = new System.Drawing.Point(197, 53);
+            this.lblFabricant.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblFabricant.Name = "lblFabricant";
-            this.lblFabricant.Size = new System.Drawing.Size(69, 16);
+            this.lblFabricant.Size = new System.Drawing.Size(57, 13);
             this.lblFabricant.TabIndex = 6;
             this.lblFabricant.Text = "Fabricant :";
             // 
             // cbFabricant
             // 
             this.cbFabricant.FormattingEnabled = true;
-            this.cbFabricant.Location = new System.Drawing.Point(338, 62);
+            this.cbFabricant.Location = new System.Drawing.Point(254, 50);
+            this.cbFabricant.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cbFabricant.Name = "cbFabricant";
-            this.cbFabricant.Size = new System.Drawing.Size(179, 24);
+            this.cbFabricant.Size = new System.Drawing.Size(135, 21);
             this.cbFabricant.TabIndex = 7;
             // 
             // lblUnite
             // 
             this.lblUnite.AutoSize = true;
-            this.lblUnite.Location = new System.Drawing.Point(40, 112);
+            this.lblUnite.Location = new System.Drawing.Point(30, 91);
+            this.lblUnite.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblUnite.Name = "lblUnite";
-            this.lblUnite.Size = new System.Drawing.Size(44, 16);
+            this.lblUnite.Size = new System.Drawing.Size(38, 13);
             this.lblUnite.TabIndex = 8;
             this.lblUnite.Text = "Unité :";
             // 
             // cbUnite
             // 
             this.cbUnite.FormattingEnabled = true;
-            this.cbUnite.Location = new System.Drawing.Point(90, 104);
+            this.cbUnite.Location = new System.Drawing.Point(68, 84);
+            this.cbUnite.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cbUnite.Name = "cbUnite";
-            this.cbUnite.Size = new System.Drawing.Size(51, 24);
+            this.cbUnite.Size = new System.Drawing.Size(39, 21);
             this.cbUnite.TabIndex = 9;
             // 
             // btnAnnuler
             // 
-            this.btnAnnuler.Location = new System.Drawing.Point(123, 159);
+            this.btnAnnuler.Location = new System.Drawing.Point(92, 129);
+            this.btnAnnuler.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAnnuler.Name = "btnAnnuler";
-            this.btnAnnuler.Size = new System.Drawing.Size(75, 23);
+            this.btnAnnuler.Size = new System.Drawing.Size(56, 19);
             this.btnAnnuler.TabIndex = 10;
             this.btnAnnuler.Text = "Annuler";
             this.btnAnnuler.UseVisualStyleBackColor = true;
+            this.btnAnnuler.Click += new System.EventHandler(this.btnAnnuler_Click);
             // 
             // btnAjouter
             // 
-            this.btnAjouter.Location = new System.Drawing.Point(292, 159);
+            this.btnAjouter.Location = new System.Drawing.Point(219, 129);
+            this.btnAjouter.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAjouter.Name = "btnAjouter";
             this.btnAjouter.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.btnAjouter.Size = new System.Drawing.Size(75, 23);
+            this.btnAjouter.Size = new System.Drawing.Size(56, 19);
             this.btnAjouter.TabIndex = 11;
             this.btnAjouter.Text = "Ajouter";
             this.btnAjouter.UseVisualStyleBackColor = true;
@@ -147,9 +160,9 @@
             // 
             // FrNouvelArticle
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(557, 216);
+            this.ClientSize = new System.Drawing.Size(418, 176);
             this.Controls.Add(this.btnAjouter);
             this.Controls.Add(this.btnAnnuler);
             this.Controls.Add(this.cbUnite);
@@ -162,7 +175,6 @@
             this.Controls.Add(this.lblLibelle);
             this.Controls.Add(this.txtRef);
             this.Controls.Add(this.lblRef);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FrNouvelArticle";
             this.Text = "Ajouter un nouvel article";
             this.Load += new System.EventHandler(this.FrNouvelArticle_Load);
--- page d'accueil/FrNouvelArticle.cs
+++ page d'accueil/FrNouvelArticle.cs
@@ -51,5 +51,10 @@ namespace page_d_accueil
             }
 
         }
+
+        private void btnAnnuler_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
