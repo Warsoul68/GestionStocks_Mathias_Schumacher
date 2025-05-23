7c8348da7ee5d8303750fa56c0725d260b139a78
Mathias.Schumacher <guzmer68@gmail.com>
2025-05-23 08:12:29 +0200

    petite modif

--- page d'accueil/Connexion.Designer.cs
+++ page d'accueil/Connexion.Designer.cs
@@ -42,7 +42,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(63, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(59, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(126, 127);
             this.pictureBox1.TabIndex = 0;
